0baf99174a9b4a73f097ff8204b4feebf5b1b33d
Kononiuk <hmasteraaa@gmail.com>
2024-09-02 14:42:05 +0300

    implement metamask login

--- project-components/frontend/src/App.tsx
+++ project-components/frontend/src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+//@ts-nocheck
+import { useEffect, useState } from 'react';
 import './App.css';
+import { notifyInfo } from './utils/ToastifyInfo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [walletAddress, setWalletAddress] = useState('');
+   const connectWallet = async () => {
+      if (
+         typeof window != 'undefined' &&
+         typeof window.ethereum != 'undefined'
+      ) {
+         try {
+            /* MetaMask is installed */
+            const accounts = await window.ethereum.request({
+               method: 'eth_requestAccounts'
+            });
+            setWalletAddress(accounts[0]);
+            console.log(accounts[0]);
+         } catch (err: any) {
+            console.error(err.message);
+         }
+      } else {
+         /* MetaMask is not installed */
+         notifyInfo('MetaMask is not installed', '🙈', 'top-center');
+         console.log('Please install MetaMask');
+      }
+   };
+
+   const getCurrentWalletConnected = async () => {
+      if (
+         typeof window != 'undefined' &&
+         typeof window.ethereum != 'undefined'
+      ) {
+         try {
+            const accounts = await window.ethereum.request({
+               method: 'eth_accounts'
+            });
+            if (accounts.length > 0) {
+               setWalletAddress(accounts[0]);
+               console.log(accounts[0]);
+            } else {
+               console.log('Connect to MetaMask using the Connect button');
+            }
+         } catch (err) {
+            console.error(err.message);
+         }
+      } else {
+         /* MetaMask is not installed */
+         notifyInfo('MetaMask is not installed', '🙈', 'top-center');
+         console.log('Please install MetaMask');
+      }
+   };
+
+   const addWalletListener = async () => {
+      if (
+         typeof window != 'undefined' &&
+         typeof window.ethereum != 'undefined'
+      ) {
+         window.ethereum.on('accountsChanged', (accounts) => {
+            setWalletAddress(accounts[0]);
+            console.log(accounts[0]);
+         });
+      } else {
+         /* MetaMask is not installed */
+         setWalletAddress('');
+         notifyInfo('MetaMask is not installed', '🙈', 'top-center');
+         console.log('Please install MetaMask');
+      }
+   };
+
+   useEffect(() => {
+      getCurrentWalletConnected();
+      addWalletListener();
+   }, [walletAddress]);
+
+   return (
+      <div className='w-full h-screen bg-gray-900 flex items-center justify-center'>
+         {walletAddress && walletAddress.length > 0 ? (
+            <p className='text-gray-100'>
+               Your address: {walletAddress.substring(0, 6)}...
+               {walletAddress.substring(38)}
+            </p>
+         ) : (
+            <button
+               onClick={connectWallet}
+               className='bg-purple-700 rounded-xl text-white flex items-center justify-center text-center px-8 py-4 hover:bg-purple-800 text-3xl'
+            >
+               Connect wallet
+            </button>
+         )}
+      </div>
+   );
 }
 
 export default App;
